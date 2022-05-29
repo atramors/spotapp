22d8a23180d173eecdd8f3ed0cc714b1ca0ade29
Roman D <blackworkarts@gmail.com>
2022-05-29 22:55:56 +0300

    update tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,10 @@
 import pytest
+from httpx import AsyncClient
 from fastapi.testclient import TestClient
 
 import spotapp
+from api.models import Base
+from api.db import async_engine
 
 
 @pytest.fixture
@@ -9,4 +12,15 @@ def client():
     """Clien fixture"""
 
     with TestClient(spotapp.app) as client_fixture:
+        yield client_fixture
+
+
+@pytest.fixture
+async def async_client():
+    """Async client fixture"""
+
+    async with async_engine.begin() as connection:
+        await connection.run_sync(Base.metadata.create_all)
+
+    async with AsyncClient(app=spotapp.app, base_url="http://test") as client_fixture:
         yield client_fixture
