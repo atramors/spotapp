c0938f055a95c404da7ee867a861fa527ef21f4a
Roman D <blackworkarts@gmail.com>
2022-05-31 23:05:57 +0300

    initial working get-post user method

--- api/crud.py
+++ api/crud.py
@@ -3,7 +3,7 @@ from sqlalchemy import select, text
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from api.models import UserDBModel
-from api.schema import UserModel
+from api.schema import UserSchema
 
 
 class CRUDSpotApp:
@@ -11,26 +11,15 @@ class CRUDSpotApp:
 
     async def get_user_by_user_id(self,
                                   db: AsyncSession,
-                                  user_id: int,
+                                  id: int,
                                   ) -> Dict:
-        query = select(self.model).filter(self.model.user_id == user_id)
+        query = select(self.model).filter(self.model.user_id == id)
         result = await db.execute(query)
 
         return result.mappings().one()
 
     async def post_user(self,
                         db: AsyncSession,
-                        user: UserModel
+                        user: UserSchema
                         ) -> Dict:
-        # query = users.insert().values(text=note.text, completed=note.completed)
-
-        query = text(
-            """INSERT INTO table_name
-            (nickname, first_name, last_name, user_pic, email,
-            hashed_password, friends, spot_photos, added_spots, favourite_spots,
-            premium_account_type)
-               VALUES (value1, value2, …)
-               RETURNING username;"""
-        )
-        result = await db.execute(query)
-        return {**user.dict(), "id": result}
+        return db.add(user)
--- api/routes.py
+++ api/routes.py
@@ -6,7 +6,8 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from api.constants import API_TITLE
 from api.crud import CRUDSpotApp
 from api.db import get_session
-from api.schema import UserModel, Error
+from api.schema import UserSchema, Error
+from api.models import UserDBModel
 
 
 spotapp_router = APIRouter(tags=[API_TITLE])
@@ -15,7 +16,7 @@ logger = logging.getLogger(__name__)
 
 @spotapp_router.get(
     path="/user/{user_id}",
-    response_model=UserModel,
+    # response_model=UserSchema,
     responses={
         200: {"description": "requested user by user_id"},
         404: {"model": Error, "description": "requested user was not found"},
@@ -25,6 +26,27 @@ logger = logging.getLogger(__name__)
 async def get_user(user_id: int,
                    db: AsyncSession = Depends(get_session),
                    ):
-    result = await CRUDSpotApp.get_user_by_user_id(db=db,
-                                                   user_id=user_id)
+    result = await CRUDSpotApp().get_user_by_user_id(db=db,
+                                                     id=user_id)
+    return result
+
+
+@spotapp_router.post(
+    path="/signup",
+)
+async def create_user(request: UserSchema,
+                      db: AsyncSession = Depends(get_session),):
+    new_user = UserDBModel(**request.dict())
+        # nickname=request.nickname,
+        #                    first_name=request.first_name,
+        #                    last_name=request.last_name,
+        #                    user_pic=request.user_pic,
+        #                    email=request.email,
+        #                    hashed_password=request.hashed_password,
+        #                    premium_account_type=request.premium_account_type)
+    # import pdb; pdb.set_trace()
+    # query = UserDBModel.insert().values(new_user)
+
+    result = await CRUDSpotApp().post_user(db=db, user=new_user)
+
     return result
