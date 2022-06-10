fd33daf20a9c718b00d0bbe7ca024b2d507c6b5b
Roman D <blackworkarts@gmail.com>
2022-06-10 21:23:59 +0300

    add hasher

---
+++ api/utils.py
@@ -0,0 +1,11 @@
+from passlib.context import CryptContext
+
+
+class PasswordHasher():
+    pass_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+    def verify_password(self, plain_password, hashed_password):
+        return self.pass_context.verify(plain_password, hashed_password)
+
+    def hash_password(self, password):
+        return self.pass_context.hash(password)
