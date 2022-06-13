1336a600f58640b46a68983d63275a0f08dd14a0
Roman D <blackworkarts@gmail.com>
2022-06-13 10:32:44 +0300

    updated autospec

--- tests/test_spotapp_user.py
+++ tests/test_spotapp_user.py
@@ -5,7 +5,7 @@ from tests import stubs, sample
 
 
 def test_user_get_by_id_ok(client, mocker):
-    mocker.patch.object(CRUDUser, "get_user_by_id", side_effect=stubs.get_user_by_id_stub, auto_spec=True)
+    mocker.patch.object(CRUDUser, "get_user_by_id", side_effect=stubs.get_user_by_id_stub, autospec=True)
     response = client.get("/users/1")
     assert response.status_code == HTTPStatus.OK
     assert response.json() == sample.EXAMPLE_USER_GET
