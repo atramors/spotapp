9b50487b20e884d42998f7070cd71227bd299c58
Roman D <blackworkarts@gmail.com>
2022-06-13 22:15:40 +0300

    add get all users test

--- tests/stubs.py
+++ tests/stubs.py
@@ -1,8 +1,16 @@
-
 from sqlalchemy.ext.asyncio import AsyncSession
 from tests import sample
+
+
+async def get_user_by_id_empty_stub(db: AsyncSession,
+                                    user_id: int):
+    return []
 
 
 async def get_user_by_id_stub(db: AsyncSession,
                               user_id: int):
     return sample.EXAMPLE_USER_GET
+
+
+async def get_all_users_stub(db: AsyncSession):
+    return [sample.EXAMPLE_USER_GET, sample.EXAMPLE_USER_GET]
--- tests/test_spotapp_user.py
+++ tests/test_spotapp_user.py
@@ -17,7 +17,14 @@ def test_user_get_by_id_ok(client, mocker):
 
 
 def test_user_get_by_id_422(client, mocker):
-    mocker.patch.object(CRUDUser, "get_user_by_id", side_effect=stubs.get_user_by_id_stub, autospec=True)
+    mocker.patch.object(CRUDUser, "get_user_by_id", side_effect=stubs.get_user_by_id_empty_stub, autospec=True)
     response = client.get("/users/wrong_data_type")
     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
     assert response.json() == sample.EXAMPLE_USER_422
+
+
+def test_user_get_all_ok(client, mocker):
+    mocker.patch.object(CRUDUser, "get_all_users", side_effect=stubs.get_all_users_stub, autospec=True)
+    response = client.get("/users/all/")
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == [sample.EXAMPLE_USER_GET, sample.EXAMPLE_USER_GET]
