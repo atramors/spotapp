294c6f1426580686b06bb6e15c60d7aa3acdb34e
Roman D <blackworkarts@gmail.com>
2022-06-13 10:19:00 +0300

    update

--- tests/stubs.py
+++ tests/stubs.py
@@ -3,5 +3,6 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from tests import sample
 
 
-async def get_user_by_id_stub(db: AsyncSession, user_id: int):
+async def get_user_by_id_stub(db: AsyncSession,
+                              user_id: int):
     return sample.EXAMPLE_USER_GET
